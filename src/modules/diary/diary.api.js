e8e93ebdce31f713663d87615da10740553ff811
aaneitchik <aaneitchik@gmail.com>
2020-07-11 17:05:06 +0300

    Reduce nesting in database structure by getting rid of the `day` entity
    No real value in it except grouping the entries in a card. This also comes as a preparation for Edit functionality, it will be easier since it's the entity that we'll be editing, not the entire day.

--- src/modules/diary/diary.api.js
+++ src/modules/diary/diary.api.js
@@ -3,12 +3,12 @@ import firebase from 'firebase/app';
 import { getDb } from '../../firebase';
 import { ENTRY_TYPES } from '../../common/types';
 
-const getUserDaysRef = async () => {
+const getUserEntriesRef = async () => {
   const userId = firebase.auth().currentUser.uid;
 
   const db = await getDb();
 
-  return db.collection('days').doc(userId).collection('days');
+  return db.collection('userEntries').doc(userId).collection('entries');
 };
 
 const diaryApi = {
@@ -20,14 +20,46 @@ const diaryApi = {
 export default diaryApi;
 
 async function getAll() {
-  const userDaysRef = await getUserDaysRef();
-  const querySnapshot = await userDaysRef
-    .orderBy('datetime', 'desc')
-    // TODO: Limit is temporary, will need proper loading by page
-    .limit(14)
+  const userEntriesRef = await getUserEntriesRef();
+  const daysToLoad = 7;
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const date = today.getDate();
+  const dateFilter =
+    new Date(year, month, date).getTime() / 1000 -
+    (daysToLoad - 1) * 24 * 60 * 60;
+
+  const querySnapshot = await userEntriesRef
+    .where('datetime', '>=', dateFilter)
     .get();
 
-  return querySnapshot.docs.map(doc => doc.data());
+  // This is an example of reduce method which is better with param reassigning.
+  // It's also fine cause the data comes from the API.
+  /* eslint-disable no-param-reassign */
+  const daysByDate = querySnapshot.docs
+    .map(doc => ({ id: doc.id, ...doc.data() }))
+    .reduce((days, entry) => {
+      const day = days[entry.datetime];
+      if (day) {
+        day.entries.push(entry);
+      } else {
+        days[entry.datetime] = {
+          datetime: entry.datetime,
+          entries: [entry],
+        };
+      }
+
+      return days;
+    }, {});
+  /* eslint-enable */
+
+  return Object.values(daysByDate)
+    .map(day => ({
+      ...day,
+      entries: day.entries.sort((a, b) => a.order - b.order),
+    }))
+    .sort((a, b) => b.datetime - a.datetime);
 }
 
 // Would be better to get this from the server, but for now here is ok
@@ -47,16 +79,7 @@ function getEntryTypes() {
 }
 
 async function addEntry(entry) {
-  // Since each entry corresponds to a date, we'll use it as an id
-  const entryId = entry.datetime.toString();
+  const userEntriesRef = await getUserEntriesRef();
 
-  const userDaysRef = await getUserDaysRef();
-  // Create an entry if it doesn't exist, update otherwise
-  return userDaysRef.doc(entryId).set(
-    {
-      datetime: entry.datetime,
-      entries: firebase.firestore.FieldValue.arrayUnion(entry),
-    },
-    { merge: true }
-  );
+  return userEntriesRef.doc().set(entry);
 }
--- src/pages/diary/DayCard.js
+++ src/pages/diary/DayCard.js
@@ -5,26 +5,24 @@ import './DayCard.css';
 import { toLocaleDate } from '../../common/formatting.utils';
 import NoteEntry from './NoteEntry';
 import FoodEntry from './FoodEntry';
+import { ENTRY_TYPES } from '../../common/types';
 
 const DayCard = ({ day }) => {
   return (
     <article className="day">
       <h1 className="day__date">{toLocaleDate(day.datetime)}</h1>
-      {/* TODO: Actually index is not a good idea, but for now we don't have any editing. */}
-      {/* Replace with something like an id when necessary */}
-      {/* eslint-disable react/no-array-index-key */}
-      {day.entries.map((entry, index) => {
+      {day.entries.map(entry => {
         let entryToRender = null;
 
-        if (entry.type === 'NOTE') {
+        if (entry.type === ENTRY_TYPES.NOTE) {
           entryToRender = <NoteEntry entry={entry} />;
-        } else if (entry.type === 'EATING') {
+        } else if (entry.type === ENTRY_TYPES.EATING) {
           entryToRender = <FoodEntry entry={entry} />;
         }
 
         if (entryToRender) {
           return (
-            <div className="--mb-1" key={index}>
+            <div className="--mb-1" key={entry.id}>
               {entryToRender}
             </div>
           );
@@ -32,7 +30,6 @@ const DayCard = ({ day }) => {
 
         return null;
       })}
-      {/* eslint-enable react/no-array-index-key */}
     </article>
   );
 };
